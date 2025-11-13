3394af2cb85a12f45ee0fa94b2cbd0af4822720c
Unai Gonzalez <unaigsanchez@gmail.com>
2025-11-13 01:34:54 +0100

    small fix on loading page

--- frontend/src/components/SkillSection.jsx
+++ frontend/src/components/SkillSection.jsx
@@ -121,23 +121,20 @@ import translations from "./Translator.jsx";
 
 export default function SkillSection({ title, isReversed }) {
   const { skillsTools } = translations();
-  const auxTitle = title;
-  if (title == "Tools") {
-    title = skillsTools;
-  }
+  const displayTitle = title == "Tools" ? skillsTools : title;
 
   return (
     <>
       <div
-        className={`flex flex-col md:flex${
-          isReversed ? "-row-reverse" : "-row"
+        className={`flex flex-col ${
+          isReversed ? "md:flex-row-reverse" : "md:flex-row"
         } group hover:bg-bg-primary border-b border-accent-primary`}
       >
         <div className="bg-bg-terniary p-6 w-full md:w-1/4 text-md flex items-center justify-center group-hover:bg-bg-secondary md:text-xl md:min-h-96">
-          {title}
+          {displayTitle}
         </div>
         <div className="flex flex-wrap justify-evenly items-center gap-8 m-8 md:m-16 text-center md:w-1/2 md:gap-20 md:mx-auto">
-          {Object.entries(techsData[auxTitle]).map(([k, v]) => (
+          {Object.entries(techsData[title]).map(([k, v]) => (
             <a
               key={k}
               className="flex flex-col p-1 w-20 rounded-md items-center hover:bg-bg-terniary active:bg-bg-terniary"
