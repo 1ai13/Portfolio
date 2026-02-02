84a6375571b0e1f68aa3e527bc3eda97dc1b632c
Unai Gonzalez <unaigsanchez@gmail.com>
2026-02-02 23:22:49 +0100

    fixing git icon bug

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -40,7 +40,10 @@ export default function ProjectCard({ title, desc, tags, logo, links }) {
                   target="_blank"
                   className="flex flex-col items-center rounded-3xl p-2 w-18 hover:bg-bg-secondary active:bg-bg-secondary hover:scale-110 transition-all duration-200"
                 >
-                  <img className="w-10" src={l.source + "-icon.png"}></img>
+                  <img
+                    className="w-10"
+                    src={l.source.toLowerCase() + "-icon.png"}
+                  ></img>
                   {l.source}
                 </a>
               );
