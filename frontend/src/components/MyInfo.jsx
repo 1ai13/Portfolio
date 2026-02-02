3ff953af37094c7950c2f7246b77c9ea528d3f9e
Unai Gonzalez <unaigsanchez@gmail.com>
2026-02-02 22:19:16 +0100

    deploying web

--- frontend/src/components/MyInfo.jsx
+++ frontend/src/components/MyInfo.jsx
@@ -114,7 +114,7 @@ function MyInfo() {
   useEffect(() => {
     const id = setTimeout(() => {
       setIsLoaded(true);
-      // getGitData();
+      getGitData();
     }, 2650);
 
     return () => clearTimeout(id);
